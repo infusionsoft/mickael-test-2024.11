0c5813cbd2f15cd10fd1e68bf96f7b56eeb21af1
Keap API team <api@keap.com>
2025-01-24 17:42:25 +0000

    Updating SDK 0.0.112 [skip ci]

--- sdks/core/java/src/main/java/com/keap/sdk/sdk/Configuration.java
+++ sdks/core/java/src/main/java/com/keap/sdk/sdk/Configuration.java
@@ -14,7 +14,7 @@
 package com.keap.sdk.sdk;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.10.0")public class Configuration {
-    public static final String VERSION = "0.0.111";
+    public static final String VERSION = "0.0.112";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
--- sdks/core/javascript/src/keap.sdk.core/ApiClient.js
+++ sdks/core/javascript/src/keap.sdk.core/ApiClient.js
@@ -16,7 +16,7 @@ import superagent from "superagent";
 
 /**
 * @module keap.sdk.core/ApiClient
-* @version 0.0.111
+* @version 0.0.112
 */
 
 /**
@@ -54,7 +54,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            'User-Agent': 'core-service-core-javascript/0.0.111'
+            'User-Agent': 'core-service-core-javascript/0.0.112'
         };
 
         /**

31f4f89e9e91850d5944b109bc657a35c91cabf3
Keap API team <api@keap.com>
2025-01-29 16:04:12 +0000

    Updating SDK 0.0.125 [skip ci]

--- sdks/core/python/keap_sdk_core_client/configuration.py
+++ sdks/core/python/keap_sdk_core_client/configuration.py
@@ -503,7 +503,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.70.0.770650\n"\
-               "SDK Package Version: 0.0.124".\
+               "SDK Package Version: 0.0.125".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:

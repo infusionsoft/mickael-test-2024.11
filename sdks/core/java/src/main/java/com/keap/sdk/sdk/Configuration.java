7e2479f240ece071485d980491b755bf350ac2fc
Keap API team <api@keap.com>
2025-02-03 00:10:40 +0000

    Updating SDK 0.0.136 [skip ci]

--- sdks/core/java/src/main/java/com/keap/sdk/sdk/Configuration.java
+++ sdks/core/java/src/main/java/com/keap/sdk/sdk/Configuration.java
@@ -14,7 +14,7 @@
 package com.keap.sdk.sdk;
 
 @jakarta.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.10.0")public class Configuration {
-    public static final String VERSION = "0.0.135";
+    public static final String VERSION = "0.0.136";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
--- sdks/core/javascript/src/keap.sdk.core/index.js
+++ sdks/core/javascript/src/keap.sdk.core/index.js
@@ -384,7 +384,7 @@ import TagsApi from './api/TagsApi';
 * </pre>
 * </p>
 * @module keap.sdk.core/index
-* @version 0.0.135
+* @version 0.0.136
 */
 export {
     /**

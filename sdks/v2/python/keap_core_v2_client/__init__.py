4b9c1c5ae1089c9deb5aafee260c8e1218e196ec
Keap API team <api@keap.com>
2025-02-11 16:26:57 +0000

    Updating SDK 0.0.149 [skip ci]

--- sdks/v2/python/keap_core_v2_client/__init__.py
+++ sdks/v2/python/keap_core_v2_client/__init__.py
@@ -14,7 +14,7 @@
     Do not edit the class manually.
 """  # noqa: E501
 
-__version__ = "0.0.148"
+__version__ = "0.0.149"
 
 # import apis into sdk package
 from keap_core_v2_client.api.affiliate_api import AffiliateApi

aaaab536b52b1802ce06bebf012e183922130180
Keap API team <api@keap.com>
2025-02-11 00:10:49 +0000

    Updating SDK 0.0.147 [skip ci]

--- sdks/v2/php/lib/Configuration.php
+++ sdks/v2/php/lib/Configuration.php
@@ -100,7 +100,7 @@ class Configuration
      *
      * @var string
      */
-    protected $userAgent = 'core-service-v2-php/0.0.146';
+    protected $userAgent = 'core-service-v2-php/0.0.147';
 
     /**
      * Debug switch (default set to false)
@@ -433,7 +433,7 @@ class Configuration
         $report .= '    OS: ' . php_uname() . PHP_EOL;
         $report .= '    PHP Version: ' . PHP_VERSION . PHP_EOL;
         $report .= '    The version of the OpenAPI document: 2.70.0.772488' . PHP_EOL;
-        $report .= '    SDK Package Version: 0.0.146' . PHP_EOL;
+        $report .= '    SDK Package Version: 0.0.147' . PHP_EOL;
         $report .= '    Temp Folder Path: ' . self::getDefaultConfiguration()->getTempFolderPath() . PHP_EOL;
 
         return $report;

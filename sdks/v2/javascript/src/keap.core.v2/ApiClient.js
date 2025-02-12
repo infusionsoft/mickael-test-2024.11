cf4fd1eeac14953305e38186e2794b2fbb9a27d3
Keap API team <api@keap.com>
2025-02-12 00:10:58 +0000

    Updating SDK 0.0.152 [skip ci]

--- sdks/v2/javascript/src/keap.core.v2/ApiClient.js
+++ sdks/v2/javascript/src/keap.core.v2/ApiClient.js
@@ -16,7 +16,7 @@ import superagent from "superagent";
 
 /**
 * @module keap.core.v2/ApiClient
-* @version 0.0.151
+* @version 0.0.152
 */
 
 /**
@@ -54,7 +54,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            'User-Agent': 'core-service-v2-javascript/0.0.151'
+            'User-Agent': 'core-service-v2-javascript/0.0.152'
         };
 
         /**
--- sdks/v2/javascript/src/keap.core.v2/index.js
+++ sdks/v2/javascript/src/keap.core.v2/index.js
@@ -384,7 +384,7 @@ import TagsApi from './api/TagsApi';
 * </pre>
 * </p>
 * @module keap.core.v2/index
-* @version 0.0.151
+* @version 0.0.152
 */
 export {
     /**

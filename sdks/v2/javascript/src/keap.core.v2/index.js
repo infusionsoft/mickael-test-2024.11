3f685e78a9ec1dfc33aaa531ef3067e83289c2ed
Keap API team <api@keap.com>
2025-02-14 17:55:16 +0000

    Updating SDK 0.0.158 [skip ci]

--- sdks/v2/javascript/src/keap.core.v2/index.js
+++ sdks/v2/javascript/src/keap.core.v2/index.js
@@ -386,7 +386,7 @@ import TagsApi from './api/TagsApi';
 * </pre>
 * </p>
 * @module keap.core.v2/index
-* @version 0.0.157
+* @version 0.0.158
 */
 export {
     /**
--- sdks/v2/python/keap_core_v2_client/__init__.py
+++ sdks/v2/python/keap_core_v2_client/__init__.py
@@ -14,7 +14,7 @@
     Do not edit the class manually.
 """  # noqa: E501
 
-__version__ = "0.0.157"
+__version__ = "0.0.158"
 
 # import apis into sdk package
 from keap_core_v2_client.api.affiliate_api import AffiliateApi

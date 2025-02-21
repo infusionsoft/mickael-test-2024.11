2c738f745637f539dc5f2bd40cc6bfe2598cd81a
Keap API team <api@keap.com>
2025-02-21 18:47:09 +0000

    Updating SDK 0.0.172 [skip ci]

--- sdks/v2/javascript/src/keap.core.v2/ApiClient.js
+++ sdks/v2/javascript/src/keap.core.v2/ApiClient.js
@@ -16,7 +16,7 @@ import superagent from "superagent";
 
 /**
 * @module keap.core.v2/ApiClient
-* @version 0.0.171
+* @version 0.0.172
 */
 
 /**
@@ -54,7 +54,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            'User-Agent': 'core-service-v2-javascript/0.0.171'
+            'User-Agent': 'core-service-v2-javascript/0.0.172'
         };
 
         /**
--- sdks/v2/python/keap_core_v2_client/__init__.py
+++ sdks/v2/python/keap_core_v2_client/__init__.py
@@ -14,7 +14,7 @@
     Do not edit the class manually.
 """  # noqa: E501
 
-__version__ = "0.0.171"
+__version__ = "0.0.172"
 
 # import apis into sdk package
 from keap_core_v2_client.api.affiliate_api import AffiliateApi

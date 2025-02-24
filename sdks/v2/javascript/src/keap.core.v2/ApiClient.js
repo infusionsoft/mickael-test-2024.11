00f5a332e7367df5a1d47b43cb400b3b48e3c389
Keap API team <api@keap.com>
2025-02-24 00:12:46 +0000

    Updating SDK 0.0.175 [skip ci]

--- sdks/v2/javascript/src/keap.core.v2/ApiClient.js
+++ sdks/v2/javascript/src/keap.core.v2/ApiClient.js
@@ -16,7 +16,7 @@ import superagent from "superagent";
 
 /**
 * @module keap.core.v2/ApiClient
-* @version 0.0.174
+* @version 0.0.175
 */
 
 /**
@@ -54,7 +54,7 @@ class ApiClient {
          * @default {}
          */
         this.defaultHeaders = {
-            'User-Agent': 'core-service-v2-javascript/0.0.174'
+            'User-Agent': 'core-service-v2-javascript/0.0.175'
         };
 
         /**
--- sdks/v2/python/keap_core_v2_client/configuration.py
+++ sdks/v2/python/keap_core_v2_client/configuration.py
@@ -503,7 +503,7 @@ class Configuration:
                "OS: {env}\n"\
                "Python Version: {pyversion}\n"\
                "Version of the API: 2.70.0.778988\n"\
-               "SDK Package Version: 0.0.174".\
+               "SDK Package Version: 0.0.175".\
                format(env=sys.platform, pyversion=sys.version)
 
     def get_host_settings(self) -> List[HostSetting]:
